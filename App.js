9d4c327dae2d837cd57584583bbfb763ec7223d6
Felipe <felipealvescosta@live.com>
2021-09-12 20:27:24 -0300

    chore: update readme

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+
+const slides = [
+  {
+    key: "1",
+    title: "E aí meu patrão!",
+    image: require("./src/assets/1.png"),
+  },
+  {
+    key: "2",
+    title: "Tá fazendo o quê hoje?",
+    image: require("./src/assets/2.png"),
+  },
+  {
+    key: "3",
+    title: "Bora tirar essa ideia do papel ?!",
+    image: require("./src/assets/3.png"),
+  },
+];
 
 export default function App() {
+  function handleOnboarding({ item }) {
+    return (
+      <View style={{ flex: 1 }}>
+        <Image source={item.image} style={styles.image} />
+        <Text style={styles.text}>{item.title}</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppIntroSlider
+      style={styles.container}
+      data={slides}
+      renderItem={handleOnboarding}
+      activeDotStyle={{
+        backgroundColor: "#ffffffff",
+        width: 30,
+      }}
+      dotStyle={{
+        backgroundColor: "#ffffffff",
+      }}
+      renderNextButton={() => <Text style={styles.button}>Próximo</Text>}
+      renderDoneButton={() => <Text style={styles.button}>Protinho!</Text>}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
   },
+  image: { height: "75%", width: "100%" },
+  text: {
+    color: "#ffffff",
+    fontSize: 30,
+    textAlign: "center",
+    paddingLeft: "10%",
+    paddingRight: "10%",
+    fontWeight: "bold",
+  },
+  button: { color: "#ffffff", fontSize: 20, paddingTop: 7 },
 });
---
+++ README.md
@@ -0,0 +1,82 @@
+<p align="center">
+
+  <h2 align="center">A Simple OnBoading App </h2>
+
+  <p align="center">
+    <br />
+    <small>A simple OnBoard App using react-native-intro-slider lib</small>
+    <br />
+
+  </p>
+</p>
+
+## Contents
+
+- [Contents](#contents)
+- [About The Project](#about-the-project)
+  - [Built With](#built-with)
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+- [Contributing](#contributing)
+- [Screenshots](#screenshots)
+- [Contact](#contact)
+
+## About The Project
+
+<p align="center">
+  <img src="src/assets/OnBoarding.gif" width="100%" />
+</p>
+
+<br />
+
+### Built With
+
+-   [React - v.17.0.2](https://pt-br.reactjs.org/)
+-   [ReactNative - v.0.65.1](https://reactnative.dev/)
+-   [React Native App Intro Slider - v.4.0.4 ](https://github.com/Jacse/react-native-app-intro-slider)
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Installation
+
+1. Clone the repo
+
+```sh
+git clone https://github.com/felipealvescosta/onboard-app
+```
+
+2. Install packages
+
+```sh
+yarn install
+```
+
+3. Run Project
+
+```sh
+react-native run-ios or react-native run-android
+```
+
+
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/YourFeature`)
+3. Commit your Changes (`git commit -m 'Add some YourFeature'`)
+4. Push to the Branch (`git push origin feature/YourFeature`)
+5. Open a Pull Request
+
+## Screenshots
+
+<p align="center">
+  <img src="src/assets/banner.png" width="100%" />
+</p>
+
+## Contact
+
+[Felipe Alves](https://felipealvescosta.com) - [LinkedIn](https://www.linkedin.com/in/felipealvesdacosta/) - [GitHub](https://github.com/felipealvescosta)
